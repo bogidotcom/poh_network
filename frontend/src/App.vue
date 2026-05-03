4f4171e6a27a315fb1a98112e3eb92fba328f777
Bogdan <sizov.workingbox@gmail.com>
2026-05-03 18:51:56 +0400

    add deck link

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,7 +8,7 @@ import Landing from './components/Landing.vue'
   <footer class="site-footer">
     <a href="https://t.me/poh_network_group" target="_blank" rel="noopener" class="footer-link">Telegram</a>
     <a href="https://x.com/poh_network" target="_blank" rel="noopener" class="footer-link">X / Twitter</a>
-    <a href="https://proofofhuman.ge" target="_blank" rel="noopener" class="footer-link">Deck</a>
+    <a href="https://drive.google.com/file/d/1hsO9V9VYTZ9pPcbgvYX6zJG_dSid1vfS/view?usp=sharing" target="_blank" rel="noopener" class="footer-link">Deck</a>
   </footer>
 </template>
 
